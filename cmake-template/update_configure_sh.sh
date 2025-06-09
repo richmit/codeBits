d46504093906987720bf17106f37dd2371a10c01
Mitch Richling <http://www.mitchr.me/>
2025-06-09 16:11:27 -0500

    Added update_configure_sh.sh

---
+++ cmake-template/update_configure_sh.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This is just a quick and dirty way to update the configure.sh script across several projects
+
+dirs=(~/world/my_prog/mraster/ ~/world/my_prog/MRMathCPP/ ~/world/my_prog/MRPTree/ ~/world/my_prog/FuncViz/)
+
+for d in ${dirs[*]}; do
+  if [ -d $d ]; then
+    if diff -q $d/configure.sh configure.sh; then
+      echo update_configure_sh.sh: Configure script is up to date: $d
+    else
+      echo update_configure_sh.sh: Updateing configure script: $d
+      cp configure.sh $d/configure.sh
+    fi
+  else
+    echo update_configure_sh.sh: Directory missing: $d
+  fi
+done
