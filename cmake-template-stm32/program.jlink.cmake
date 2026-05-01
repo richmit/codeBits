8c570deaabb86120afd879e8f167a07fd3fc4859
Mitch Richling <11151403+richmit@users.noreply.github.com>
2026-05-01 14:30:51 -0500

    jlink support

---
+++ cmake-template-stm32/program.jlink.cmake
@@ -0,0 +1,8 @@
+Device @STM32_DEVICE@
+SelectInterface SWD
+Speed 4000
+connect
+loadFile @PROJECT_NAME@.elf
+Reset
+Go
+Quit
--- cmake-template-stm32/programViaJLink.cmake
+++ cmake-template-stm32/programViaJLink.cmake
@@ -1,10 +1,12 @@
 # -*- Mode:cmake; Coding:us-ascii-unix; fill-column:158 -*-
 #########################################################################################################################################################.H.S.##
 ##
-# @file      stm32STprogram.cmake
+# @file      stm32programJLink.cmake
 # @author    Mitch Richling http://www.mitchr.me/
-# @brief     Program a typical Nuculeo board with an ST-Link probe.@EOL
-# @keywords  stm32 cortex-m microcontroller embedded STM32_Programmer_CLI
+# @date      2026-05-01
+# @version   VERSION
+# @brief     @EOL
+# @keywords  
 # @std       cmake
 # @see       https://github.com/richmit/codeBits/
 # @copyright 
@@ -28,57 +30,44 @@
 #  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 #  DAMAGE.
 #  @endparblock
+# @todo      @EOL@EOL
+# @warning   @EOL@EOL
+# @bug       @EOL@EOL
 # @filedetails
 #
-#  We expect STM32_Programmer_CLI to be on the PATH.  This is known to be true in two important cases:
-#   - Inside of /STM32CubeIDE/ and /STM32Cube for Visual Studio Code Extension/.
-#   - When using the /STM32Cube Bundles Manager/ with a "Cube Bundle Project".
-# If we can't find it on the PATH, then we look for it via the cube wrapper.
+#  File details go here.  Multiple paragraphs are fine....
 #
 #########################################################################################################################################################.H.E.##
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------
-# Program board
-
-# Search for programmer on the PATH for windows platforms
-find_program(STM32_PROG_BIN 
-  "STM32_Programmer_CLI.exe"
-  PATHS "C:/Program Files/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin/")
-
-# Search for programmer on the PATH for MacOS & linux platforms
-find_program(STM32_PROG_BIN 
-  "STM32_Programmer_CLI"
-  PATHS "/opt/ST/STM32Cube/STM32CubeProgrammer"
-        "/opt/ST/STM32CubeProgrammer"
-        "~/ST/STM32Cube/STM32CubeProgrammer"
-        "~/ST/STM32CubeProgrammer"
-        "/Applications/STMicroelectronics/STM32Cube/STM32CubeProgrammer/STM32CubeProgrammer.app/Contents/MacOs/bin")
-
-# Search for the programmer via the cube wrapper
-if(NOT (EXISTS "${STM32_PROG_BIN}"))
-  message(STATUS "STM32 Programmer binary was not found on PATH.  Searching for cube bundle manager!")
-  execute_process(
-    COMMAND cube "--resolve" "programmer"
-    OUTPUT_VARIABLE TMP_STRING
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
-  string(REGEX REPLACE "^Command: '" "" TMP_STRING "${TMP_STRING}")
-  string(REGEX REPLACE "'.*$" "" TMP_STRING "${TMP_STRING}")
-  set(STM32_PROG_BIN "${TMP_STRING}")
-endif()
-
-# Report Results & Create program targte if we can.
-if(EXISTS "${STM32_PROG_BIN}")
-  # Program Target Board
-  message(STATUS "Found STM32 Programmer binary is ${STM32_PROG_BIN}")
-  add_custom_target(program
-    COMMAND ${STM32_PROG_BIN} -c port=SWD -w ${PROJECT_NAME}.elf -v -rst
-    DEPENDS ${PROJECT_NAME}.elf
-    COMMENT "Programming device."
-  )
-  message(STATUS "'program' target created.")
+#
+# Create program target for J-Link
+#
+# Look for installed JLink versions on Windows in the default install path
+file(GLOB JLINK_ALL_VER
+  "C:/Program Files/SEGGER/JLink_V*"
+)
+#
+# Find the newest version of JLink installed
+if(JLINK_ALL_VER)
+  list(SORT JLINK_ALL_VER)
+  list(GET JLINK_ALL_VER -1 JLINK_NEW_VER)
+  set(STM32_JLINK_PROG_BIN ${JLINK_NEW_VER}/JLink.exe)
 else()
-  message(STATUS "Unable to find STM32 Programmer binary!")
+  message(STATUS "No SEGGER J-Link Install Found!")
+endif()
+#
+# Report Results & Create program targte if we can.
+if(EXISTS "${STM32_JLINK_PROG_BIN}")
+  message(STATUS "Found J-Link binary: ${STM32_JLINK_PROG_BIN}")
+  CONFIGURE_FILE(${CMAKE_SOURCE_DIR}/program.jlink.cmake ${CMAKE_BINARY_DIR}/program.jlink)
+  add_custom_target(program-jlink
+    COMMAND ${STM32_JLINK_PROG_BIN} -NoGui 1 -CommandFile program.jlink 
+    DEPENDS ${PROJECT_NAME}.elf
+    COMMENT "Programming device with J-Link."
+  )
+  message(STATUS "'program-jlink' target created.")
+else()
+  message(STATUS "Unable to find J-Link binary!")
   message(WARNING "No 'program' target will be created.")
 endif()
-
