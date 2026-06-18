d5bc7f69b6518864379a260833bb99a062fa93f5
Mitch Richling <11151403+richmit@users.noreply.github.com>
2026-06-18 15:54:50 -0500

    C++ & JSON fixes

--- templates/template.hpp
+++ templates/template.hpp
@@ -1,12 +1,20 @@
 #ifndef MJR_INCLUDE_FILENAME-EXT-UC
 
+namespace mjr {
+
+  namespace MJR_INCLUDE_FILENAME-EXT {
+
 #ifdef __cplusplus
-extern "C" {
+    extern "C" {
 #endif
 
 #ifdef __cplusplus
-}
+    } // extern "C"
 #endif
+
+  } // namespace: MJR_INCLUDE_FILENAME-EXT
+
+} // namespace: mjr
 
 #define MJR_INCLUDE_FILENAME-EXT-UC
 #endif
