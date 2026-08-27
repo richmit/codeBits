08fee67856b12a55f0d0c885ae1a07b75cd82b99
Mitch Richling <11151403+richmit@users.noreply.github.com>
2026-08-27 13:51:48 -0500

    New: bin/assocate_dir_with_github_repo.sh

---
+++ bin/assocate_dir_with_github_repo.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env -S sh
+# -*- Mode:Shell-script; Coding:us-ascii-unix; fill-column:158 -*-
+#########################################################################################################################################################.H.S.##
+##
+# @file      assocate_dir_with_github_repo.sh
+# @author    Mitch Richling https://www.mitchr.me/
+# @brief     Setup a directory for use with GitHub.@EOL
+# @keywords  git
+# @std       sh
+# @see       https://github.com/richmit/
+# @copyright 
+#  @parblock
+#  Copyright (c) 2026, Mitchell Jay Richling <https://www.mitchr.me/> All rights reserved.
+#  
+#  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+#  
+#  1. Redistributions of source code must retain the above copyright notice, this list of conditions, and the following disclaimer.
+#  
+#  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions, and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#  
+#  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software
+#     without specific prior written permission.
+#  
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+#  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+#  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+#  DAMAGE.
+#  @endparblock
+#########################################################################################################################################################.H.E.##
+
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------
+ACT='T'
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------
+REPONAME=$(basename $(pwd))
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------
+if [ -e .git ]; then
+  echo git remote add origin git@github.com:richmit/$REPONAME.git
+  if [ $ACT == "T" ]; then git remote add origin git@github.com:richmit/$REPONAME.git; fi
+  echo git branch -M main
+  if [ $ACT == "T" ]; then git branch -M main; fi
+  echo git push -u origin main
+  if [ $ACT == "T" ]; then git push -u origin main; fi
+  echo git config --local user.email '11151403+richmit@users.noreply.github.com'
+  if [ $ACT == "T" ]; then git config --local user.email '11151403+richmit@users.noreply.github.com';  fi
+else
+  echo git init
+  if [ $ACT == "T" ]; then git init; fi
+
+  README_FILE=""
+  for f in README.org README.md README.txt; do
+    if [ -e $f ]; then
+      README_FILE=$f
+      break
+    fi
+  done
+  if [ -z "$README_FILE" ]; then
+    README_FILE=README.org
+    echo $REPONAME >> $README_FILE
+  fi
+
+  echo git add $README_FILE
+  if [ $ACT == "T" ]; then git add $README_FILE; fi
+
+  echo git config --local user.email '11151403+richmit@users.noreply.github.com'
+  if [ $ACT == "T" ]; then git config --local user.email '11151403+richmit@users.noreply.github.com';  fi
+
+  echo git commit -m "Initial README Commit"
+  if [ $ACT == "T" ]; then git commit -m "Initial README Commit"; fi
+
+  echo git branch -M main
+  if [ $ACT == "T" ]; then git branch -M main; fi
+  echo git remote add origin git@github.com:richmit/$REPONAME.git
+  if [ $ACT == "T" ]; then git remote add origin git@github.com:richmit/$REPONAME.git; fi
+  echo git push -u origin main
+  if [ $ACT == "T" ]; then git push -u origin main; fi
+fi
